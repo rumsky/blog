db25bec419cf126525ee1e1bdbfcc4446b75148c
rumsky <hongyu.zhao@bitmain.com>
2019-10-31 15:02:55 +0800

    style webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,44 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Image from "../images/qrcode.jpg"
 import SEO from "../components/seo"
+import "../styles/base.css"
+import "../styles/index.css"
 
 const IndexPage = () => (
-  <Layout>
+  <div className="container">
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="header">
+      <div className="title">不唯前端</div>
+      <div>
+        <span>ABOUT</span>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <div className="content">
+      <div className="types">
+        <div className="content-title">
+          PROJECTS
+          <div className="underline"></div>
+        </div>
+        <ul>
+          <li>React</li>
+          <li>Next</li>
+          <li>Flutter</li>
+          <li>Blockchain</li>
+          <li>UI Design</li>
+        </ul>
+      </div>
+      <img src={Image} alt="微信公众号：不唯前端" className="qr-code" />
+      <div></div>
+    </div>
+    <div className="footer">
+      <div className="footer-right">
+        <p style={{ marginTop: 0, marginBottom: 0 }}>水光潋滟晴方好</p>
+        <p>山色空蒙雨亦奇</p>
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage
